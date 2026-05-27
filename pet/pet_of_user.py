4b4828ef789fa9a9dad82ce2126c8e504fd83fd3
Kevin-Aguilar816 <aguilarvhonkevinjames@gmail.com>
2026-05-27 21:20:20 +0800

    created a set pet animal type method for the user to input the pet animal type

--- pet/pet_of_user.py
+++ pet/pet_of_user.py
@@ -6,6 +6,11 @@ def set_pet_name(Pet, name):
     return pet
 
 
+def set_pet_animal_type(Pet, animal_type):
+    pet = Pet(animal_type)
+    return pet
+
+
 if __name__ == "__main__":
-    name = input("Enter your pet's name:")
-    pet = set_pet_name(Pet, name)
+    name = input("Enter your pet's name: ")
+    animal_type = input("Enter your pet's animal type: ")
