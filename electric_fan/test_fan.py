3840866b04d7b633e2bbfa8e1c08043766f2883f
Kevin-Aguilar816 <aguilarvhonkevinjames@gmail.com>
2026-05-27 08:37:14 +0800

    created the second fan and fixed the logic error of 'true' in fan one

--- electric_fan/test_fan.py
+++ electric_fan/test_fan.py
@@ -2,4 +2,5 @@ from fan_class import Fan
 
 
 def test_fan():
-    fan_one = Fan(fan.fast, True, 10.0, 'yellow')
+    fan_one = Fan(fan.fast, False, 10.0, 'yellow')
+    fan_two = Fan(fan.medium, True,  5.0, 'blue')
