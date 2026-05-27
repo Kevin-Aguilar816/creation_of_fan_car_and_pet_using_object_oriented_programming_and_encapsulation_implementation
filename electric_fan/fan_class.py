d3b8740042a2a7dd515136fa06d27bfb43bb3f30
Kevin-Aguilar816 <aguilarvhonkevinjames@gmail.com>
2026-05-27 08:25:43 +0800

    completed the constructor to the default values given by the project requirement

--- electric_fan/fan_class.py
+++ electric_fan/fan_class.py
@@ -3,14 +3,14 @@ class Fan:
     medium = 2
     fast = 3
 
-    def __init__(self, speed, is_on, radius, color):
+    def __init__(self, speed=slow, is_on=False, radius=5.0, color='blue')):
         self.__speed = speed
         self.__is_on = is_on
         self.__radius = radius
         self.__color = color
 
-    def get_speed(self):
+        def get_speed(self):
         return self.__speed
 
-    def set_speed(self, speed):
+        def set_speed(self, speed):
         self.__speed = speed
