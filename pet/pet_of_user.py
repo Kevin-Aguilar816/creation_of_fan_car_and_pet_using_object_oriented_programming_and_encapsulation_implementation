f22b05982753370e0cda1ffea14196aba25dec6d
Kevin-Aguilar816 <aguilarvhonkevinjames@gmail.com>
2026-05-27 21:44:42 +0800

    created a method that prompts the user to input the pet's name, animal type, and age as well as display those information. Also gave initial values for name, animal type, and age at the pet class constructor

--- pet/pet_of_user.py
+++ pet/pet_of_user.py
@@ -1,16 +1,15 @@
 from pet_class import Pet
 
 
-def set_pet_name(Pet, name):
-    pet = Pet(name)
-    return pet
-
-
-def set_pet_animal_type(Pet, animal_type):
-    pet = Pet(animal_type)
+def pet_of_user():
+    pet = Pet()
+    pet.set_name(input("Enter your pet's name: "))
+    pet.set_animal_type(input("Enter your pet's animal type: "))
+    pet.set_age(float(input("Enter your pet's age: ")))
     return pet
 
 
 if __name__ == "__main__":
-    name = input("Enter your pet's name: ")
-    animal_type = input("Enter your pet's animal type: ")
+    pet = pet_of_user()
+    print(
+        f"Your pet's name is {pet.get_name()}, it is a {pet.get_animal_type()}, and it is {pet.get_age()} years old.")
