77ccb05746042aa784b7e10709435e6915948122
Kevin-Aguilar816 <aguilarvhonkevinjames@gmail.com>
2026-05-27 21:13:50 +0800

    abolished the recently created class and created a set pet name method that prompts the user to input the pet name

--- pet/pet_of_user.py
+++ pet/pet_of_user.py
@@ -1,4 +1,11 @@
 from pet_class import Pet
 
 
-class PetOfUser(Pet):
+def set_pet_name(Pet, name):
+    pet = Pet(name)
+    return pet
+
+
+if __name__ == "__main__":
+    name = input("Enter your pet's name:")
+    pet = set_pet_name(Pet, name)
