53730f3e9b1d600623eee0b045552bf4a120d80a
Kevin-Aguilar816 <aguilarvhonkevinjames@gmail.com>
2026-05-28 22:42:04 +0800

    fixed some strings to make the code logically correct

--- interactive_pet_car_electric_fan_objects.py
+++ interactive_pet_car_electric_fan_objects.py
@@ -15,7 +15,7 @@ def car_accelerate(Car, Fan, Pet):
     while Car.get_speed() != "25 km/h":
         Car.accelerate()
         print(
-            f"\nMy {my_car.get_year_model()} {my_car.get_make()} has a speed of {Car.get_speed()} km/h")
+            f"\nMy {my_car.get_year_model()} {my_car.get_make()} has a speed of {Car.get_speed()}")
 
     fan_one.set_is_on(False)
     print(f"\n{my_pet.get_name()} enjoyed the {fan_one.get_color()} fan at {fan_one.get_speed()} speed while my {my_car.get_year_model()} {my_car.get_make()} accelerated!")
@@ -25,10 +25,10 @@ def car_brake(Car, Fan):
     while Car.get_speed() != "0 km/h":
         Car.brake()
         print(
-            f"\nMy {my_car.get_year_model()} {my_car.get_make()} has a speed of {Car.get_speed()} km/h")
+            f"\nMy {my_car.get_year_model()} {my_car.get_make()} has a speed of {Car.get_speed()}")
 
     fan_two.set_is_on(True)
-    print(f"\nmy {my_car.get_year_model()} {my_car.get_make()} has stopped and my {fan_two.get_color()} fan is still off.")
+    print(f"\nMy {my_car.get_year_model()} {my_car.get_make()} has stopped and my {fan_two.get_color()} fan is still off.")
 
 
 if __name__ == "__main__":
@@ -37,9 +37,9 @@ if __name__ == "__main__":
     fan_one = Fan("fast", True, 10.0, "yellow")
     fan_two = Fan("medium", False, 5.0, "blue")
 
-    print(f"\nHi there! {my_pet.get_name()} is a {my_pet.get_animal_type()} and is {my_pet.get_age()} years old.\nWe have a {my_car.get_year_model()} {my_car.get_make()} that has {fan_one.get_radius()} {fan_one.get_color()} fan and {fan_two.get_radius()} {fan_two.get_color()} fan that can have {fan_two.get_speed()} speed.")
+    print(f"\nHi there! I'm with {my_pet.get_name()}. {my_pet.get_name()} is a {my_pet.get_age()} year-old {my_pet.get_animal_type()}.\nWe have a {my_car.get_year_model()} {my_car.get_make()} that has {fan_one.get_radius()} radius {fan_one.get_color()} fan and {fan_two.get_radius()} radius {fan_two.get_color()} fan that can reach {fan_two.get_speed()} speed.")
     print(
-        f"\nInitial speed of my {my_car.get_year_model()} {my_car.get_make()} is {my_car.get_speed()}")
+        f"\nThe initial speed of my {my_car.get_year_model()} {my_car.get_make()} is {my_car.get_speed()}")
     print(f"\nThe {fan_one.get_color()} fan is {fan_one.get_is_on()} for {my_pet.get_name()} to enjoy its {fan_one.get_speed()} speed while my {my_car.get_year_model()} {my_car.get_make()} is accelerating.")
     print(
         f"\nThe {fan_two.get_color()} fan is {fan_two.get_is_on()} for me as I am cool myself.")
