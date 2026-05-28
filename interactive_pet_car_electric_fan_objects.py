83531ee96987bfaf88f6e2d1bbf3e2645c6bc57e
Kevin-Aguilar816 <aguilarvhonkevinjames@gmail.com>
2026-05-28 19:18:17 +0800

    Created a car accelerate method that makes the car accelerate while the fan 'cools' the pet

--- interactive_pet_car_electric_fan_objects.py
+++ interactive_pet_car_electric_fan_objects.py
@@ -9,3 +9,12 @@ def get_pet():
     pet.set_animal_type(input("Enter your pet's animal type: "))
     pet.set_age(float(input("Enter your pet's age: "))
     return pet
+
+def car_accelerate(car, fan_one, pet):
+    while car.get_speed() != "25 km/h":
+        car.accelerate()
+        print(
+            f"My {my_car.get_year_model()} {my_car.get_make()} has a speed of ({car.get_speed()} km/h)")
+
+    fan_one.set_is_on(True)
+    print(f"{pet.get_name()} is enjoying the fan at {fan_one.get_speed()} speed while my {my_car.get_year_model()} {my_car.get_make()} is accelerating!")
