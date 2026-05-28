4c73866b2b8f258fb9510faf283d40316a0b0e3a
Kevin-Aguilar816 <aguilarvhonkevinjames@gmail.com>
2026-05-28 19:10:53 +0800

    created a get pet method that gets the pet's info like its name, animal type, and age

--- interactive_pet_car_electric_fan_objects.py
+++ interactive_pet_car_electric_fan_objects.py
@@ -1,3 +1,11 @@
 from fan_class import Fan
 from car_class import Car
 from pet_class import Pet
+
+
+def get_pet():
+    pet = Pet()
+    pet.set_name(input("Enter your pet's name: "))
+    pet.set_animal_type(input("Enter your pet's animal type: "))
+    pet.set_age(float(input("Enter your pet's age: "))
+    return pet
